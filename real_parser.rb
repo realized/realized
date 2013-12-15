bff829c71e0e877fe05fcb95d54ed1a441b32610
Tim Reddehase <tredd@informatik.uni-bremen.de>
2013-12-15 15:48:56 +0100

    run the parser on the testfile with nice output.

--- real_parser.rb
+++ real_parser.rb
@@ -16,4 +16,20 @@ end
 require 'parser'
 require 'processor'
 
+#######################################################################
+#                          Application Portion                        #
+#                                                                     #
+#######################################################################
 
+REAL_TEST_FILE = APP_ROOT.join('test/fixtures/reversible_circuit.real')
+
+file = REAL_TEST_FILE.open('r')
+real_input = file.read
+file.close
+
+begin
+  parsed = REAL::Parser.new.parse(real_input)
+  puts parsed
+rescue Parslet::ParseFailed => failure
+  puts failure.cause.ascii_tree
+end
