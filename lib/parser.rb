dd068510ed2a3297210068efb57d91160596d6c3
Tim Reddehase <tredd@informatik.uni-bremen.de>
2013-12-15 14:21:36 +0100

    start parsing with a 'version'-rule.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,36 @@
 module REAL
   class Parser < Parslet::Parser
 
+    # Building Block rules #
+    ########################
+
+    rule(:space) { str(' ') }
+    rule(:dot) { str('.') }
+    rule(:number) { match('[0-9]').repeat(1) }
+    rule(:significant_number) { number >> dot >> number }
+    rule(:line_end) { str("\n") }
+
+    # Specific Parts - Rules #
+    ##########################
+
+    rule(:version) do
+      str('.version') >>
+        space >>
+        significant_number >>
+      line_end
+    end
+
+    # Meta-Rules #
+    ##############
+
+    # meta-rule that "puts it all together"
+    rule (:file) { version >> rest.repeat }
+
+    # catch-all rule, only for debugging/testing purposes
+    rule(:rest) { match(".|\n") }
+
+    # root rule, start parsing here
+    root(:file)
+
   end
 end
