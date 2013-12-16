1b3a11ba6185b89f5a08a08aa3b2e33d8a7b536f
Tim Reddehase <tredd@informatik.uni-bremen.de>
2013-12-16 23:20:23 +0100

    extract code/gem loading into own file.

---
+++ loader.rb
@@ -0,0 +1,18 @@
+require 'bundler'
+
+Bundler.require
+
+# stdlib requires
+require 'pathname'
+
+APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
+
+%w{lib}.each do |directory|
+  # add directory to load path
+  # (so you can require files with require directly)
+  $: << APP_ROOT.join(directory).to_s
+end
+
+require 'parser'
+require 'processor'
+
