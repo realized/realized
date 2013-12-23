7e811249f766ef8b1f03eddb99684c887cc314db
Tim Reddehase <tredd@informatik.uni-bremen.de>
2013-12-23 17:04:14 +0100

    adjust loader to load a sinatra app.

---
+++ config.ru
@@ -0,0 +1,3 @@
+require_relative 'config/loader.rb'
+
+run Realized
--- config/loader.rb
+++ config/loader.rb
@@ -5,9 +5,9 @@ Bundler.require
 # stdlib requires
 require 'pathname'
 
-APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
-%w{lib}.each do |directory|
+%w{app lib config}.each do |directory|
   # add directory to load path
   # (so you can require files with require directly)
   $: << APP_ROOT.join(directory).to_s
