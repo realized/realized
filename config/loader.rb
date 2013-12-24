675e9fc242d91ef76b83ddbf6aa14466816b9fdd
Tim Reddehase <tredd@informatik.uni-bremen.de>
2013-12-24 13:11:59 +0100

    simplify loading of files
    Now every file in 'lib/real' is loaded.
    We also load every file-descendant of 'lib/settings'.

--- config/loader.rb
+++ config/loader.rb
@@ -7,17 +7,22 @@ require 'pathname'
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
+module Loader
+  def self.load_in(relative_to_app_root)
+    Dir.glob(APP_ROOT.join(relative_to_app_root, '**/*.rb')).each do |f|
+      require f
+    end
+  end
+end
+
 %w{app lib config}.each do |directory|
   # add directory to load path
   # (so you can require files with require directly)
   $: << APP_ROOT.join(directory).to_s
 end
 
-require 'real/inner_parser'
-require 'real/parser'
-require 'real/processor'
+Loader.load_in('lib/real')
+Loader.load_in('lib/settings')
 
-require 'settings/base'
-require 'settings/provide'
 require 'render_helper'
 require 'realized'
