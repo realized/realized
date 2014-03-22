08af1febb1c2afa8a684d868ee1b3210856bbd44
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-03-22 23:25:45 +0100

    fetch the circuit data and draw it
    Fetch the circuit data (parsing on server-side)
    via ajax and draw the circuit after that.
    Also remove now obsolete fixtures in code.

---
+++ public/js/draw_circuit.js
@@ -0,0 +1,18 @@
+var draw = function(circuit, lines){
+  window.drawer = new Drawer((circuit.length+2)*50, 500, circuit, lines);
+  window.drawer.draw_circuit();
+  window.drawer.convert_to_svg();
+};
+
+$(document).ready(function(){
+  var file = "sym9_147.real";
+  // Drawing the circuit automatically overwrites
+  // this message.
+  $("#current_circuit_drawing").
+    append($("<p/>").
+      html("Currently parsing the circuit..."));
+  $.get("parsed/"+file, function(data){
+      draw(data.circuit, data.variables);
+  }, "json");
+});
+
