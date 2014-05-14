078eb6ecd7d897a16e1a20b8fb5f4bddfbb5ef57
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-05-14 20:50:36 +0200

    refactor file-code for explicit hook-registering.

--- public/js/circuit_loading.js
+++ public/js/circuit_loading.js
@@ -1,3 +1,8 @@
+var store = null;
+
+var retrieve_local_files = function() {
+  return store.keys().map(store.retrieve_raw);
+};
 
 var draw = function(circuit, lines){
   window.drawer = new Drawer((circuit.length+2)*50, 500, circuit, lines);
@@ -21,59 +26,64 @@ var recreate_file_list = function(files) {
   });
 }
 
-$(document).ready(function(){
-  var store = new LocalStore('realized:parse');
+var register_server_files_submit_hook = function() {
+  $('form#real_files_selector').submit(function(e) {
+    e.preventDefault();
+    var file = $(this).find('select option:selected').val();
+    $.get("parsed/"+file, draw_circuit, "json");
+  });
+}
 
-  var retrieve_local_files = function() {
-    return store.keys().map(store.retrieve_raw);
-  };
+var register_upload_file_submit_hook = function() {
+  var button = $('form#real_file_uploader input[type="submit"]');
+  var button_value = button.val();
+  $('input#real_file_select').fileupload({
+    url: '/parse/',
+    type: 'POST',
+    dataType: 'json',
+    done: function (e, data) {
+      var response = data.result;
+      store.store(response.filename, response);
+      $(button).attr('value', button_value);
+      draw_circuit(response);
+      $('ul#real_file_selected_files li').remove();
+    },
+    add: function (e, data) {
+      recreate_file_list(data.files);
+      data.context = button.on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        button_value = $(this).val();
+        $(this).attr('value', 'Uploading...');
+        data.submit();
+      });
+    }
+  });
+}
+
+var register_local_files_submit_hook = function() {
+  $('form#real_local_files_selector').submit(function(e) {
+    e.preventDefault();
+    var file = $(this).find('select option:selected').val();
+    var data = store.retrieve(file);
+    draw_circuit(data);
+  });
+};
+
+$(document).ready(function(){
+  store = new LocalStore('realized:parse');
 
   $.get("available_files", function(data){
-      Template.use('file_selection', function(template) {
-        var template_data = {
-          files: data,
-          local_files: retrieve_local_files(),
-        };
-        $("div#circuit_loading").html(template(template_data));
+    Template.use('file_selection', function(template) {
+      var template_data = {
+        files: data,
+        local_files: retrieve_local_files(),
+      };
+      $("div#circuit_loading").html(template(template_data));
 
-        $('form#real_files_selector').submit(function(e) {
-          e.preventDefault();
-          var file = $(this).find('select option:selected').val();
-          $.get("parsed/"+file, draw_circuit, "json");
-        });
-
-        var button = $('form#real_file_uploader input[type="submit"]');
-        var button_value = button.val();
-        $('input#real_file_select').fileupload({
-          url: '/parse/',
-          type: 'POST',
-          dataType: 'json',
-          done: function (e, data) {
-            var response = data.result;
-            store.store(response.filename, response);
-            $(button).attr('value', button_value);
-            draw_circuit(response);
-            $('ul#real_file_selected_files li').remove();
-          },
-          add: function (e, data) {
-            recreate_file_list(data.files);
-            data.context = button.on('click', function(e) {
-              e.preventDefault();
-              e.stopPropagation();
-              button_value = $(this).val();
-              $(this).attr('value', 'Uploading...');
-              data.submit();
-            });
-          }
-        });
-
-        $('form#real_local_files_selector').submit(function(e) {
-          e.preventDefault();
-          var file = $(this).find('select option:selected').val();
-          var data = store.retrieve(file);
-          draw_circuit(data);
-        });
-
-      });
+      register_server_files_submit_hook();
+      register_upload_file_submit_hook();
+      register_local_files_submit_hook();
+    });
   }, "json");
 });
